eb6337a4c97962ddf3468ed2081275aa5434c0c4
plaktos <plaktos@protonmail.com>
2018-07-23 02:14:48 +0200

    test urls

--- src_test/t_dsession.cpp
+++ src_test/t_dsession.cpp
@@ -4,14 +4,14 @@
 #include "../src/dsession.hpp"
 
 TEST_CASE("DSession http download", "[download]"){
-    const char* url = ""
+    const char* url = "https://github.com/plaktos/DMan";
     DSession::SessionType t = DSession::SessionType::http;
     DSession s(url, t);
     REQUIRE(s.start());
 }
 
 TEST_CASE("DSession txt download", "[download]"){
-    const char* url = ""
+    const char* url = "https://raw.githubusercontent.com/plaktos/DMan/master/res_test/download_test.txt";
     DSession::SessionType t = DSession::SessionType::http;
     DSession s(url, t);
     REQUIRE(s.start());
